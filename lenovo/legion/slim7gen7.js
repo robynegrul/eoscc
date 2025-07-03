42b0b443dbd7a8b9bc197b9b21e542115653fe5d
robynegrul <negruroby5@gmail.com>
2025-07-03 22:33:31 +0300

    initial commit

---
+++ lenovo/legion/slim7gen7.js
@@ -0,0 +1,148 @@
+const LENOVO_VID = 0x048D;
+const ITEDEV_PID = 0xC977;
+
+const BRIGHTNESS_MIN = 0;
+const BRIGHTNESS_MAX = 9;
+
+const REPORT_SIZE = 960;
+const REPORT_ID = 0x07;
+
+const Commands = {
+    SET_BRIGHTNESS: 0x0CE,
+    GET_BRIGHTNESS: 0x0CD,
+};
+
+const clamp = (x, a, b) => Math.min(Math.max(x, a), b);
+
+class LenovoLegionSlim7Gen7Kb {
+    #handle;
+
+    static get VID() { return LENOVO_VID; };
+    static get PID() { return ITEDEV_VID; };
+
+    constructor(hnd) {
+        this.#handle = hnd ? hnd : LenovoLegionSlim7Gen7Kb.connectAuto();
+    };
+
+    static new = async function(hnd) {
+        return new LenovoLegionSlim7Gen7Kb(
+            hnd ? hnd : await LenovoLegionSlim7Gen7Kb.connectAuto()
+        );
+    };
+
+    isOpen = function() {
+        return this.#handle && this.#handle.opened;
+    };
+
+    static connectManual = async () => {
+        try {
+            const filters = [{
+                vendorId: LENOVO_VID,
+                productId: ITEDEV_PID,
+            }];
+
+            const devs = await navigator.hid.requestDevice({filters});
+            if (devs.length === 0) {
+                console.log("No matching device selected.");
+                return null;
+            }
+
+            let hnd = devs[0];
+            await hnd.open();
+
+            console.log(`Connected to ${hnd.productName}`);
+            return hnd;
+        } catch (error) {
+            console.log(`Failed to connect: ${error.message}`);
+            return null;
+        }
+    };
+
+    static connectAuto = async () => {
+        try {
+            const devs = await navigator.hid.getDevices();
+            let hnd = devs.find(
+                (d) => d.vendorId === LENOVO_VID
+                    && d.productId === ITEDEV_PID
+            );
+
+            if (!hnd) {
+                console.log("Device needs manual connection");
+                hnd = await this.connectManual();
+                return hnd;
+            }
+
+            await hnd.open();
+            console.log(`Connectedd to ${hnd.productName}`);
+            return hnd;
+        } catch (error) {
+            console.log(`Failed to connect: ${error.message}`);
+            return null;
+        }
+    };
+
+    setBrightness = async (value) => {
+        if (!this.isOpen()) {
+            console.log("Device not connected");
+            return;
+        }
+
+        if (!(Number.isInteger(value))) {
+            console.log("Input in not a number");
+            return;
+        }
+
+        value = clamp(value, BRIGHTNESS_MIN, BRIGHTNESS_MAX);
+
+        const buffer = new Uint8Array(REPORT_SIZE);
+        buffer.set([Commands.SET_BRIGHTNESS, 0xC0, 0x03, value], 0);
+
+        try {
+            await this.#handle.sendFeatureReport(REPORT_ID, buffer)
+            console.log(`Set brightness to ${value}`);
+        } catch (error) {
+            console.log(`Failed to send feature report: ${error.message}`);
+            return;
+        }
+    };
+
+    getBrightness = async () => {
+        if (!this.isOpen()) {
+            console.log("Device not connected")
+            return;
+        }
+
+        const buffer = new Uint8Array(REPORT_SIZE);
+        buffer.set([Commands.GET_BRIGHTNESS, 0xC0, 0x03], 0);
+
+        try {
+            await this.#handle.sendFeatureReport(REPORT_ID, buffer);
+            const res = await this.#handle.receiveFeatureReport(REPORT_ID);
+            const value = (new Uint8Array(res.buffer))[4];
+            console.log(`Current brightness: ${value}`);
+            return value;
+        } catch (error) {
+            console.log(`Failed to send feature report: ${error.message}`);
+            return;
+        }
+    };
+}
+
+let bSlider = document.getElementById("brightness-slider");
+let bIndicator = document.getElementById("brightness-value");
+
+let kb = null;
+
+document.getElementById("connect-button").addEventListener("click", async () => {
+    kb = await LenovoLegionSlim7Gen7Kb.new(null);
+
+    let brightness = await kb.getBrightness();
+    bSlider.value = brightness;
+    bIndicator.textContent = brightness;
+});
+
+bSlider.addEventListener("input", async () => {
+    const value = parseInt(bSlider.value, 10);
+    bIndicator.textContent = value;
+    await kb.setBrightness(value);
+})
