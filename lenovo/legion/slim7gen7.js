df764cc504f42d6cb71728827d20c1cbe0ad3bd4
robynegrul <negruroby5@gmail.com>
2025-07-04 03:00:02 +0300

    fix default export

--- lenovo/legion/slim7gen7.js
+++ lenovo/legion/slim7gen7.js
@@ -14,7 +14,7 @@ const Commands = {
 
 const clamp = (x, a, b) => Math.min(Math.max(x, a), b);
 
-export default class LenovoLegionSlim7Gen7Kb {
+export class LenovoLegionSlim7Gen7Kb {
     #handle;
 
     static get VID() { return LENOVO_VID; };
